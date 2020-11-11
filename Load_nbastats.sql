5ab5bfee39990d0b6818790995df02224a856666
Anas Saad <71991758+AnasjSaad@users.noreply.github.com>
2020-11-10 21:50:36 -0500

    Add files via upload

---
+++ Load_nbastats.sql
@@ -0,0 +1,28 @@
+USE nba;
+CREATE TABLE nbastats(
+player_name varchar(30),
+team_abbreviation varchar(30),
+age	varchar(30),
+player_height varchar(30),
+player_weight varchar(30),	
+college	varchar(30),
+country	varchar(30),
+draft_year varchar(30),
+draft_round	varchar(30),
+draft_number varchar(30),
+gp varchar(30),
+pts varchar(30),
+reb varchar(30),
+ast varchar(30),
+net_rating varchar(30),
+oreb_pct varchar(30),
+dreb_pct varchar(30),
+usg_pct	varchar(30),
+ts_pct varchar(30),
+ast_pct varchar(30),
+season varchar(30))
+
+select * from nbastats;
+
+SELECT * from draftnumberaverage
+
